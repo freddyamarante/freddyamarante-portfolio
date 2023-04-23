af0878db94cb4bbd37a08c55ffe46abf1ee1694c
Freddy Amarante <freddyamarante14@gmail.com>
2023-04-23 01:18:03 -0400

    fix: delete unnecessary comments and unused imports

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -1,18 +1,17 @@
 import * as THREE from 'three'
-import { Wireframe, Float, CameraShake } from '@react-three/drei'
+import { CameraShake } from '@react-three/drei'
 import Sphere from './Sphere'
 
 const config = {
-  maxYaw: 0.005, // Max amount camera can yaw in either direction
-  maxPitch: 0.1, // Max amount camera can pitch in either direction
-  maxRoll: 0.1, // Max amount camera can roll in either direction
-  yawFrequency: 0.1, // Frequency of the the yaw rotation
-  pitchFrequency: 0.1, // Frequency of the pitch rotation
-  rollFrequency: 0.1, // Frequency of the roll rotation
-  intensity: 0.5, // initial intensity of the shake
-  decay: false, // should the intensity decay over time
-  decayRate: 0.65, // if decay = true this is the rate at which intensity will reduce at
-  controls: undefined, // if using orbit controls, pass a ref here so we can update the rotation
+  maxYaw: 0.005,
+  maxPitch: 0.1,
+  maxRoll: 0.1,
+  yawFrequency: 0.1,
+  pitchFrequency: 0.1,
+  rollFrequency: 0.1,
+  intensity: 0.5,
+  decay: false,
+  decayRate: 0.65,
 }
 
 export default function Experience() {
