1e3f34c3cf5cdcc0beaeac64490596b91ef12b16
Freddy Amarante <freddyamarante14@gmail.com>
2023-04-29 22:57:04 -0400

    fix: horizontal padding in contact me section

--- components/ContactMe.jsx
+++ components/ContactMe.jsx
@@ -10,7 +10,7 @@ export default function ContactMe() {
   return (
     <section>
       <div className="h-full mx-8 md:mx-auto md:my-12 md:max-w-7xl">
-        <div className="flex flex-col my-16 md:my-24 px-12 md:px-20 lg:px-36">
+        <div className="flex flex-col my-16 md:my-24 md:px-20 lg:px-36">
           <h1
             className={`${amarante.className} text-2xl md:text-4xl lg:text-5xl text-center`}
           >
