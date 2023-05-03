c2e0023a2445553543892dc610323893455a0f8b
Freddy Amarante <freddyamarante14@gmail.com>
2023-05-03 13:30:29 -0400

    feat: add headers for SEOmeta tags

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,7 +11,18 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Work in progress! | Freddy Amarante - Portfolio</title>
+        <meta
+          name="description"
+          content="Hi! I am Freddy Amarante, I'm from Venezuela and I am a 22 years old front-end developer. I have +2 years of experience in JavaScript, Vue and React development. I'm currently a freelancer but I'm looking forward working for a company now."
+        />
+        <meta
+          name="keywords"
+          content="Freddy Amarante, Freddy amarante portfolio, Freddy amarante portafolio, Freddy amarante github, Freddy amarante twitter, Freddy amarante linkedin, web developer, portfolio, next.js, react, tailwind, front end developer, freddy amarante developer"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="author" content="Freddy Amarante" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>Freddy Amarante - Portfolio | Work in progress! </title>
       </Head>
       <main className={`text-night dark:text-white`}>
         <div className="flex flex-col overflow-x-hidden">
