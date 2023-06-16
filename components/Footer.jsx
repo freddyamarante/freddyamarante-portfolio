b747c1a5ae262bcbcb9b842f8b6b293ab8ee5ba1
Freddy Amarante <freddyamarante14@gmail.com>
2023-06-16 16:00:31 -0400

    feat: add mal and phone links to footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,15 +10,19 @@ export default function Footer() {
           y: 0,
           transition: { type: 'spring', stifness: 100 },
         }}
-        whileHover={{
-          scale: 1.02,
-          transition: { type: 'spring', stifness: 300 },
-        }}
         viewport={{ once: true }}
         className="bg-night dark:bg-transparent border-2 border-night dark:border-white border-b-0 text-white mx-auto w-full max-w-5xl pt-8 pb-12"
       >
         <div className="grid grid-cols-1 sm:grid-cols-2  gap-x-2  gap-y-4 justify-center text-center text-lg font-semibold ">
-          <div className="flex justify-center  items-center gap-2">
+          <motion.a
+            initial={{ y: 10 }}
+            whileHover={{
+              y: 0,
+              transition: { type: 'spring', stiffness: 100 },
+            }}
+            href="mailto:freddyamarante14@gmail.com"
+            className="flex justify-center items-center gap-2"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -34,8 +38,16 @@ export default function Footer() {
               />
             </svg>
             freddyamarante14@gmail.com
-          </div>
-          <div className="flex justify-center items-center gap-2">
+          </motion.a>
+          <motion.a
+            initial={{ y: 10 }}
+            whileHover={{
+              y: 0,
+              transition: { type: 'spring', stiffness: 100 },
+            }}
+            href="tel:+51921331375"
+            className="flex justify-center items-center gap-2"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -50,8 +62,8 @@ export default function Footer() {
                 d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
               />
             </svg>
-            +51 921 331 375
-          </div>
+            +51 921-331-375
+          </motion.a>
         </div>
       </motion.footer>
     </div>
